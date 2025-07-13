52b649dd06ee154bc4bc3e22008718a10ad98ab3
wilsoncunningham <122071360+wilsoncunningham@users.noreply.github.com>
2025-07-13 23:20:37 +0000

    books controller. making functions

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,23 +11,24 @@ class BooksController < ApplicationController
     render({:template => "reading/index"})
   end
 
-  def show
-    the_id = params.fetch("path_id")
+  # def show
+  #   the_id = params.fetch("path_id")
 
-    matching_books = Book.where({ :id => the_id })
+  #   matching_books = Book.where({ :id => the_id })
 
-    @the_book = matching_books.at(0)
+  #   @the_book = matching_books.at(0)
 
-    render({ :template => "books/show" })
-  end
+  #   render({ :template => "books/show" })
+  # end
 
   def create
     the_book = Book.new
     the_book.title = params.fetch("query_title")
-    the_book.testament = params.fetch("query_testament")
-    the_book.description = params.fetch("query_description")
-    the_book.total_chapters = params.fetch("query_total_chapters")
-    the_book.current_readers_count = params.fetch("query_current_readers_count")
+    the_book.author = params.fetch("query_author")
+    the_book.rating = params.fetch("query_rating")
+    the_book.notes = params.fetch("query_notes")
+    the_book.start_date = params.fetch("query_start_date")
+    the_book.end_date= params.fetch("end_date")
 
     if the_book.valid?
       the_book.save
@@ -42,10 +43,11 @@ class BooksController < ApplicationController
     the_book = Book.where({ :id => the_id }).at(0)
 
     the_book.title = params.fetch("query_title")
-    the_book.testament = params.fetch("query_testament")
-    the_book.description = params.fetch("query_description")
-    the_book.total_chapters = params.fetch("query_total_chapters")
-    the_book.current_readers_count = params.fetch("query_current_readers_count")
+    the_book.author = params.fetch("query_author")
+    the_book.rating = params.fetch("query_rating")
+    the_book.notes = params.fetch("query_notes")
+    the_book.start_date = params.fetch("query_start_date")
+    the_book.end_date= params.fetch("end_date")
 
     if the_book.valid?
       the_book.save
@@ -63,13 +65,5 @@ class BooksController < ApplicationController
 
     redirect_to("/books", { :notice => "Book deleted successfully."} )
   end
-
-  def read_book
-    book_id = params.fetch("book_id")
-    @book = Book.find(book_id)
-    render({:template => "/reading/book"})
-  end
-
-
 
 end
