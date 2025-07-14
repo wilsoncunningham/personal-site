4544f03d7bc8cd277e25856b4fd49f0cd7327eaa
wilsoncunningham <122071360+wilsoncunningham@users.noreply.github.com>
2025-07-14 06:30:23 +0000

    completed admin features for adding books. need to be able to edit and delete

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,5 @@
 class BooksController < ApplicationController
 
-  http_basic_authenticate_with(
-    name: ENV["ADMIN_USER"],
-    password: ENV["ADMIN_PASSWORD"],
-    only: [:create, :update, :destroy]
-  )
-
   def index
     matching_books = Book.all
 
