261ba8d9259b42bfc74ee09af4eed14afc58a0c1
Wilson Cunningham <122071360+wilsoncunningham@users.noreply.github.com>
2025-11-11 02:53:30 +0000

    added admin CRUD to photos

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -24,6 +24,19 @@ class PhotosController < ApplicationController
       render :new, status: :unprocessable_entity
     end
   end
+
+  def edit
+    @photo = Photo.find(params[:id])
+  end
+
+  def update
+    @photo = Photo.find(params[:id])
+    if @photo.update(photo_params)
+      redirect_to photos_path, notice: "Photo updated successfully."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
   
   def destroy
     @photo.destroy
